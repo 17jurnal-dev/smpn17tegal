0da03c6743376504df148a54a5316f1a2fff1b23
17jurnal-dev <17jurnal@gmail.com>
2026-04-01 19:55:10 +0700

    Add caching and fetch handling in sw.js
    Implement caching for offline support in service worker.

--- sw.js
+++ sw.js
@@ -1,5 +1,19 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
+const CACHE_NAME = "jitu-app-v1";
+const urlsToCache = [
+  "/smpn17tegal/",
+  "/smpn17tegal/index.html"
+];
+
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache=>cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', e => {});
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request)
+      .then(res=> res || fetch(e.request))
+  );
+});
